22279311cd9bae5fc15ae8f9a8dd8531bde99ed6
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-12 19:20:44 +0200

    ✨ F02:ORDERPAGE>Add redirection and usernane on component.

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,9 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 function OrderPage() {
+    //1- State(Données)
+    const { username } = useParams()
+
+    //2- Comportements
+    //3- Affichage(render)
+
     return (
         <div>
-            <h1>Bonjour</h1>
+            <h1>Bonjour {username} </h1>
             <Link to="/">
                 <button>Déconnexion</button>
             </Link>
