c97ef3459e30fd4241c7edf995590b6737d892a6
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-12 13:04:17 +0200

    ♻️ F01:LOGINPAGE>Add loginform.

--- src/components/pages/login/Loginpage.jsx
+++ src/components/pages/login/Loginpage.jsx
@@ -1,34 +1,14 @@
-import { useState } from 'react'
+import LoginForm from './LoginForm.jsx'
 
 function Loginpage() {
     //1- State(Données)
-    const [username, setUsername] = useState('')
     //2- Comportements
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        alert(`Bonjour ${username} `)
-        setUsername('')
-    }
-    const handleChange = (e) => {
-        setUsername(e.target.value)
-    }
-    //1- Affichage(render)
+    //3- Affichage(render)
     return (
-        <div>
-            <h1>Bienvenue chez nous!</h1>
-            <br />
-            <h2>Connectez-vous</h2>
-            <form action="submit" onSubmit={handleSubmit}>
-                <input
-                    value={username}
-                    type="text"
-                    placeholder="Entrer votre prenom..."
-                    required
-                    onChange={handleChange}
-                />
-                <button>Accédez à votre espace</button>
-            </form>
-        </div>
+        <>
+            {/* <Logo /> */}
+            <LoginForm />
+        </>
     )
 }
 export default Loginpage
