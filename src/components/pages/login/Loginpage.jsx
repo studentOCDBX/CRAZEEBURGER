cefd9b80d1d37e031883b4822bceb1ceca23fcf0
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-12 12:49:04 +0200

    ✨ F01: APP>Implement loginPage component.

--- src/components/pages/login/Loginpage.jsx
+++ src/components/pages/login/Loginpage.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
+
 function Loginpage() {
+    //1- State(Données)
+    const [username, setUsername] = useState('')
+    //2- Comportements
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        alert(`Bonjour ${username} `)
+        setUsername('')
+    }
+    const handleChange = (e) => {
+        setUsername(e.target.value)
+    }
+    //1- Affichage(render)
     return (
         <div>
             <h1>Bienvenue chez nous!</h1>
             <br />
             <h2>Connectez-vous</h2>
-            <form action="submit">
+            <form action="submit" onSubmit={handleSubmit}>
                 <input
+                    value={username}
                     type="text"
                     placeholder="Entrer votre prenom..."
                     required
+                    onChange={handleChange}
                 />
                 <button>Accédez à votre espace</button>
             </form>
