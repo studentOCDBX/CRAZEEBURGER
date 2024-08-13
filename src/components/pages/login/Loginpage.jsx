0446c17be25e9933747bd6a4d3e650f755251dec
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-13 03:05:23 +0200

    💄F03:LOGINPAGE>Add background image.

--- src/components/pages/login/Loginpage.jsx
+++ src/components/pages/login/Loginpage.jsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components'
 import Logo from '../../reusableUi/Logo.jsx'
 import LoginForm from './LoginForm.jsx'
+import burgerImg from '../../../assets/img/burgerBackground .jpg'
 
 const LoginpageStyled = styled.div`
-    background-color: #eca5f1e8;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+        url(${burgerImg});
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
     height: 100vh;
     display: flex;
     flex-direction: column;
--- src/components/reusableUi/Logo.jsx
+++ src/components/reusableUi/Logo.jsx
@@ -2,9 +2,6 @@ import styled from 'styled-components'
 
 const LogoStyled = styled.div`
     background-color: #ddff00;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
 `
 
 function Logo() {
