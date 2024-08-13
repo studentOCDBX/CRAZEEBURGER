c478be02648e83e4eb5968706fda849a10982ca2
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-13 02:39:45 +0200

    💄F03:Add layout

--- src/components/pages/login/Loginpage.jsx
+++ src/components/pages/login/Loginpage.jsx
@@ -1,15 +1,25 @@
+import styled from 'styled-components'
 import Logo from '../../reusableUi/Logo.jsx'
 import LoginForm from './LoginForm.jsx'
+
+const LoginpageStyled = styled.div`
+    background-color: #eca5f1e8;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
 
 function Loginpage() {
     //1- State(Données)
     //2- Comportements
     //3- Affichage(render)
     return (
-        <>
+        <LoginpageStyled>
             <Logo />
             <LoginForm />
-        </>
+        </LoginpageStyled>
     )
 }
 export default Loginpage
--- src/components/reusableUi/Logo.jsx
+++ src/components/reusableUi/Logo.jsx
@@ -1,4 +1,13 @@
+import styled from 'styled-components'
+
+const LogoStyled = styled.div`
+    background-color: #ddff00;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+`
+
 function Logo() {
-    return <div>Logo</div>
+    return <LogoStyled>CRAZEE-BURGER Logo</LogoStyled>
 }
 export default Logo
