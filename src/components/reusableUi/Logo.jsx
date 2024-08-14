5e4ef461891fb36ef243c83d9b0ee92069481295
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-14 23:43:04 +0200

    💄F03:REUSABLEUI>Logo>Styling logo components.

--- src/components/reusableUi/Logo.jsx
+++ src/components/reusableUi/Logo.jsx
@@ -1,10 +1,33 @@
 import styled from 'styled-components'
+import logoImg from '../../assets/img/logo.png'
+import theme from '../../theme/index.js'
 
 const LogoStyled = styled.div`
-    background-color: #ddff00;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Amatic SC', cursive;
+    font-weight: 700;
+    margin-bottom: 72px;
+    color: ${theme.colors.primary};
+    h1 {
+        font-size: 36px;
+        line-height: 36px;
+    }
+    img {
+        width: 80px;
+        height: 60px;
+    }
+    transform: scale(2.5);
 `
 
 function Logo() {
-    return <LogoStyled>CRAZEE-BURGER Logo</LogoStyled>
+    return (
+        <LogoStyled>
+            <h1>CRAZEE</h1>
+            <img src={logoImg} alt="Logo CrazyBurger" />
+            <h1>BURGER</h1>
+        </LogoStyled>
+    )
 }
 export default Logo
