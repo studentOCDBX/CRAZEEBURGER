51360594a0fe2136e31a54c80e425e14d33ce10b
Nazaire DARBOUX <studentdbx@gmail.com>
2024-08-14 13:45:59 +0200

    💄F03:LOGIN>LOGINFORM>Style input tag.

--- src/components/pages/login/Loginpage.jsx
+++ src/components/pages/login/Loginpage.jsx
@@ -4,11 +4,10 @@ import LoginForm from './LoginForm.jsx'
 import burgerImg from '../../../assets/img/burgerBackground .jpg'
 
 const LoginpageStyled = styled.div`
-    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
         url(${burgerImg});
     background-size: cover;
     background-position: center;
-    background-blend-mode: darken;
     height: 100vh;
     display: flex;
     flex-direction: column;
